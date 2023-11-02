8c697716b27d1ce18c5cfbdcd499b3879e5a94ee
Karina Bustamante <karina.bustamante@usach.cl>
2023-11-01 23:11:16 -0300

    Avance Informe, seccion Descripcion del Paradigma y Requerimiento Funcional 1

---
+++ lab2_13452929_Bustamante.pl
@@ -0,0 +1,24 @@
+% Archivo de Laboratorio 2 - Prolog
+
+% TDA Estudiante
+% Representacion: [Nombre, Pep1, Pep2, Pep3]
+
+% Constructor
+% Predicado makeAlumno/5
+% Dom: Nombre X Pep1 X Pep2 X Pep3 X Alumno
+% Meta Primaria: makeAlumno/5
+% Meta Secundaria: "Todas las reglas para el constructor"
+
+makeAlumno(Nombre, Pep1, Pep2, Pep3, [Nombre, Pep1, Pep2, Pep3]).
+
+
+
+% RF1
+
+
+
+% RF2
+
+
+
+% RF3
